23376456eaab6a99c471e861d1fbdae2ba9c4497
madmax <maksimkholin@gmail.com>
2024-11-08 00:05:37 +0300

    add presentation

---
+++ app/src/main/java/com/example/cryptoinfo/presentation/CoinViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.cryptoinfo.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptoinfo.data.repository.CoinRepositoryImpl
+import com.example.cryptoinfo.domain.GetCoinDetailedInfoUseCase
+import com.example.cryptoinfo.domain.GetCoinInfoListUseCase
+import com.example.cryptoinfo.domain.LoadDataUseCase
+import kotlinx.coroutines.launch
+
+class CoinViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = CoinRepositoryImpl(application)
+
+    private val getCoinInfoListUseCase = GetCoinInfoListUseCase(repository)
+    private val getCoinDetailedInfoUseCase = GetCoinDetailedInfoUseCase(repository)
+    private val loadDataUseCase = LoadDataUseCase(repository)
+
+    val coinInfoList = getCoinInfoListUseCase()
+
+    fun getDetailInfo(fSym: String) = getCoinDetailedInfoUseCase(fSym)
+
+    init {
+        viewModelScope.launch {
+            loadDataUseCase()
+        }
+    }
+}
