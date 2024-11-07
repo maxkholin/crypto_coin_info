bfa7bf20ad8106ca8efa1981e510b84ae0409f3c
madmax <maksimkholin@gmail.com>
2024-11-08 00:04:41 +0300

    add mapper

---
+++ app/src/main/java/com/example/cryptoinfo/data/mapper/CoinMapper.kt
@@ -0,0 +1,76 @@
+package com.example.cryptoinfo.data.mapper
+
+import com.example.cryptoinfo.data.database.CoinInfoDbModel
+import com.example.cryptoinfo.data.network.model.CoinInfoDto
+import com.example.cryptoinfo.data.network.model.CoinInfoJsonContainerDto
+import com.example.cryptoinfo.data.network.model.CoinNamesListDto
+import com.example.cryptoinfo.domain.CoinInfo
+import com.google.gson.Gson
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+class CoinMapper {
+
+    fun mapJsonContainerToListCoinInfo(jsonContainer: CoinInfoJsonContainerDto): List<CoinInfoDto> {
+        val result = mutableListOf<CoinInfoDto>()
+        val jsonObject = jsonContainer.json ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
+            for (currencyKey in currencyKeySet) {
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinInfoDto::class.java
+                )
+                result.add(priceInfo)
+            }
+        }
+        return result
+    }
+
+    fun mapNamesListToString(namesListDto: CoinNamesListDto): String {
+        return namesListDto.names?.map {
+            it.coinName?.name
+        }?.joinToString(",") ?: ""
+    }
+
+    fun mapDtoToDbModel(dto: CoinInfoDto): CoinInfoDbModel = CoinInfoDbModel(
+        fromSymbol = dto.fromSymbol,
+        toSymbol = dto.toSymbol,
+        price = dto.price,
+        lastUpdate = dto.lastUpdate,
+        highDay = dto.highDay,
+        lowDay = dto.lowDay,
+        lastMarket = dto.lastMarket,
+        imageUrl = BASE_IMAGE_URL + dto.imageUrl
+    )
+
+    fun mapDbModelToEntity(dbModel: CoinInfoDbModel): CoinInfo = CoinInfo(
+        fromSymbol = dbModel.fromSymbol,
+        toSymbol = dbModel.toSymbol,
+        price = dbModel.price,
+        lastUpdate = convertTimestampToTime(dbModel.lastUpdate),
+        highDay = dbModel.highDay,
+        lowDay = dbModel.lowDay,
+        lastMarket = dbModel.lastMarket,
+        imageUrl = dbModel.imageUrl
+    )
+
+    private fun convertTimestampToTime(timestamp: Long?): String {
+        if (timestamp == null) return ""
+        val stamp = Timestamp(timestamp * 1000)
+        val date = Date(stamp.time)
+        val pattern = "HH:mm:ss"
+        val sdf = SimpleDateFormat(pattern, Locale.getDefault())
+        sdf.timeZone = TimeZone.getDefault()
+        return sdf.format(date)
+    }
+
+    companion object {
+        private const val BASE_IMAGE_URL = "https://cryptocompare.com"
+    }
+}
